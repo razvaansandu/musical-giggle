786185cb170b2309f76a70e68a7e72944c7b35cd
DevGabry88 <panebiancogabriele8@gmail.com>
2025-10-30 15:25:22 +0100

    pagina del login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,9 @@
-import Card from "./components/Card";
-import { songs } from "./components/data.ts";
+import Card from "./components/Card";  
+import Login from "./SchermatePrincipali/loginPage"; 
 
 export default function Page() {
-  return (
-    <main className="main grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {songs.map((song, index) => (
-        <Card
-          key={index}
-          title={song.title}
-          description={song.description}
-          imageUrl={song.imageUrl}
-          numOfViews={song.numOfViews}
-          time={song.time}
-        />
-      ))}
-    </main>
-  );
+  return ( 
+      <Login />  
+  ); 
 }
- 
+  
