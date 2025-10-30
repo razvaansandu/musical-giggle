c2f71aa9128ba49bca580f70aae6c2864814941a
razvaansandu <razvaansandu@gmail.com>
2025-10-30 14:37:03 +0100

    correzzione

---
+++ src/app/api/artist/[id]/top-tracks/route.js
@@ -0,0 +1,39 @@
+
+import fetch from 'node-fetch';
+import { Buffer } from 'buffer';
+
+const clientId = '3c80f6f31cfe41fbb1e3a02175af8c83';
+const clientSecret = 'dc49d39e13dd49baac725c0d949fe8c0';
+
+async function getAccessToken() {
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
+    },
+    body: new URLSearchParams({ grant_type: 'client_credentials' })
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
+export async function GET(request, { params }) {
+  const { id } = params;
+
+  try {
+    const token = await getAccessToken();
+
+    const response = await fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks?market=IT`, {
+      headers: { 'Authorization': `Bearer ${token}` }
+    });
+
+    const data = await response.json();
+    return Response.json(data);
+
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ error: 'Errore nel prendere top tracks artista' }), { status: 500 });
+  }
+}
