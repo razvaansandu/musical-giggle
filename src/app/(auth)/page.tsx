3b4ca19fa9177add66763e61dc31ccbd02ee7cbc
DevGabry88 <panebiancogabriele8@gmail.com>
2025-11-10 13:12:37 +0100

    ciao modifche di searchBar

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useRef } from 'react';
-import styles from './home/home.module.css';
+import styles from '../home/home.module.css';
 import SpotifyHeader from 'components/SpotifyHeader'; 
 import MiniCard from 'components/MiniCard';
 
