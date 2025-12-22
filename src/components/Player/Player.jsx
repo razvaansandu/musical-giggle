70b2840a558dd231585aca09c31cb3f2193ceb39
Razvan <razvaansandu@gmail.com>
2025-12-22 15:57:23 +0100

    modificeh playlist, dispositivi, tracklist

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -615,20 +615,11 @@ export default function Player() {
         </div>
         {isSynced && !isAutoScroll && (
           <button className={styles.syncButton} onClick={scrollToActiveLine}> 
-<<<<<<< HEAD
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-music-note-beamed" viewBox="0 0 16 16">
-              <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13s1.12-2 2.5-2 2.5.896 2.5 2m9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2"/>
-              <path fillRule="evenodd" d="M14 11V2h1v9zM6 3v10H5V3z"/>
-              <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4z"/>
-            </svg>
-             Sincronizza
-=======
              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-repeat" viewBox="0 0 16 16">
   <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
   <path fillRule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
 </svg>
              Sincronizza  
->>>>>>> d9878efd75c09efe17afa6df8b5787ae39f3148c
           </button>
         )}
       </div>
